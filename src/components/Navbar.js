49df5cff5189fa629d083ba65236d6372f0e8903
Shane99dt <dushen1999thilaksha@gmail.com>
2023-02-27 17:26:58 +0100

    home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,10 +21,10 @@ const Navbar = () => {
       <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="p-4 flex flex-row items-center justify-between">
           <Link
-            href="#"
+            to={'/'}
             className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
           >
-            Flowtrail UI
+            WDR - Shop
           </Link>
           <button
             className="md:hidden rounded-lg focus:outline-none focus:shadow-outline"
