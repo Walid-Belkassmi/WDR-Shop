9ea7bfc09e70a7beaf72ed472560978fd1205b0b
Shane99dt <dushen1999thilaksha@gmail.com>
2023-02-28 09:56:30 +0100

    develop check

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Button = ({ label, onClick, type }) => {
+  // console
   return (
     <button
       className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
