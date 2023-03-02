0a0c1aca8ac11966577f3c1a1d591cbf4b9252d0
Walid <belkassmi.walid@gmail.com>
2023-03-02 10:20:02 +0100

    Homepage style + link fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,27 +10,28 @@ const Home = () => {
     // </div>
 
     <div className="h-screen bg-gray-50 flex items-center">
-      <section className="bg-cover bg-center py-32 w-full bg-[url('https://source.unsplash.com/random')]">
+      <section className="bg-cover bg-center py-32 w-full bg-[url('https://images.unsplash.com/photo-1602472097151-72eeec7a3185?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80')]">
         <div className="container mx-auto text-left text-white">
           <div className="flex items-center">
             <div className="w-1/2">
               <h1 className="text-5xl font-medium mb-6">
-                Welcome to My Agency
+                WDR le sport avant tout !
               </h1>
               <p className="text-xl mb-12">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                viverra euismod odio, gravida pellentesque urna varius vitae.
+                Ce sont les meilleures équipes, Sie sind die allerbesten
+                Mannschaften The main event Die Meister, die Besten, les
+                meilleures équipes, the champions !
               </p>
               <Link
-                href="#"
                 className="bg-indigo-500 text-white py-4 px-12 rounded-full hover:bg-indigo-600"
+                to={'/collections'}
               >
-                Demo
+                Collections
               </Link>
             </div>
             <div className="w-1/2 pl-16">
               <img
-                src="https://source.unsplash.com/random?ux"
+                src="https://images.unsplash.com/photo-1551854304-9235bf86ef71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
                 className="h-64 w-full object-cover rounded-xl"
                 alt="Layout img"
               />
