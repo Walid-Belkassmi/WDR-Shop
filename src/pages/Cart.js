de65235ea91aea7e9fe13d83cecd458eccd59e1c
Walid <belkassmi.walid@gmail.com>
2023-03-03 13:33:51 +0100

    cart page modifications

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -23,20 +23,20 @@ const Cart = () => {
     // </style>
 
     <div className="h-screen bg-gray-100 pt-20">
-      <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+      <h1 className="mb-10 text-center text-2xl font-bold">Paniers</h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
           <CartItem
             id={'1'}
-            title={'Nike Air Max 2019'}
-            size={'36EU - 4US'}
+            title={'Paris Saint-Germain 2022/23 Stadium Domicile'}
+            size={'M'}
             src={
-              'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+              'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/2655366c-e7b0-4d03-b2a1-6a8f618be6d2/maillot-de-football-dri-fit-paris-saint-germain-2022-23-stadium-domicile-pour-T2dRkh.png'
             }
             // onClickSubstract={}
             // onClickAdd={}
             itemCount={'1'}
-            price={'120'}
+            price={'89,99'}
             currencyType={'€'}
             // onClickRemoveItem={}
           />
@@ -46,23 +46,19 @@ const Cart = () => {
 
         <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
           <div className="mb-2 flex justify-between">
-            <p className="text-gray-700">Subtotal</p>
-            <p className="text-gray-700">$129.99</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="text-gray-700">Shipping</p>
-            <p className="text-gray-700">$4.99</p>
+            <p className="text-gray-700">Sous-total</p>
+            <p className="text-gray-700">89,99 €</p>
           </div>
           <hr className="my-4" />
           <div className="flex justify-between">
             <p className="text-lg font-bold">Total</p>
             <div className="">
-              <p className="mb-1 text-lg font-bold">$134.98 USD</p>
-              <p className="text-sm text-gray-700">including VAT</p>
+              <p className="mb-1 text-lg font-bold">89,99 €</p>
+              <p className="text-sm text-gray-700">inclus la TVA</p>
             </div>
           </div>
           <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
-            Check out
+            Valider
           </button>
         </div>
       </div>
